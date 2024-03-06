0c19e058e97b41ae1f582a8f95b34e8b5bd6b124
imi-tat0r <admin@imi-tat0r.net>
2024-03-06 18:19:58 +0100

    Added NaN check and fix

--- CS2-Essentials/Extensions/QAngleExtensions.cs
+++ CS2-Essentials/Extensions/QAngleExtensions.cs
@@ -6,6 +6,10 @@ public static class QAngleExtensions
     {
         return float.IsInfinity(angle.X) || float.IsInfinity(angle.Y) || float.IsInfinity(angle.Z);
     }
+    public static bool IsNaN(this CounterStrikeSharp.API.Modules.Utils.QAngle angle)
+    {
+        return float.IsNaN(angle.X) || float.IsNaN(angle.Y) || float.IsNaN(angle.Z);
+    }
     public static void FixInfinity(this CounterStrikeSharp.API.Modules.Utils.QAngle angle)
     {
         if (float.IsInfinity(angle.X))
@@ -13,6 +17,15 @@ public static class QAngleExtensions
         if (float.IsInfinity(angle.Y))
             angle.Y = 0;
         if (float.IsInfinity(angle.Z))
+            angle.Z = 0;
+    }
+    public static void FixNaN(this CounterStrikeSharp.API.Modules.Utils.QAngle angle)
+    {
+        if (float.IsNaN(angle.X))
+            angle.X = 0;
+        if (float.IsNaN(angle.Y))
+            angle.Y = 0;
+        if (float.IsNaN(angle.Z))
             angle.Z = 0;
     }
 }
